0d7d324e36f535e7b19aca773da54fa8ba872e5f
Soumyo Sen <soumyosen88@gmail.com>
2022-07-30 18:49:40 +0530

    Add files via upload

---
+++ pimethixene_fp.sh
@@ -0,0 +1,6 @@
+
+
+DIR=/sc/arion/projects/H_filizm02a/soumyo/serotonin_receptor/structure
+
+
+"$SCHRODINGER/utilities/canvasFPGen" -HOST Chimera-Long-lic03-2020-42:32 -TMPDIR /sc/arion/scratch/sens05/.tmp_schrodinger/  -JOB fpPim -imae $DIR/pimethixene.maegz -fptype radial -o $DIR/pimethixene.fp
